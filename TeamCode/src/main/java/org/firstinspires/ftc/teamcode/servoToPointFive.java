31a44ea53948291bdcb1e9e4e69a5ab9d239145f
Minecrft-101 <taggertallred@gmail.com>
2024-02-06 17:30:34 -0800

    gg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoToPointFive.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.SubSys.Arm;
+import org.firstinspires.ftc.teamcode.SubSys.Drive;
+import org.firstinspires.ftc.teamcode.SubSys.Stretch;
+
+@TeleOp(name = "TeleOpMain", group = "_JVBot") // YOU DID NOT HAVE THIS WRITTEN. THIS IS WHAT TELLS THE ROBOT THIS IS AN OPMODE. IMPORT WAS ALSO NOT THERE. -Nick
+public class servoToPointFive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo a = hardwareMap.get(Servo.class, "a");
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                a.setPosition(0.5);
+                telemetry.addData("arm extent correction:",a.getPosition());
+                telemetry.update();
+            }
+        }
+    }
+}
