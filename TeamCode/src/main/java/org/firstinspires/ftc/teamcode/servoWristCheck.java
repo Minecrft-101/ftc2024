bb12aefe46689ad1ffb86e2dc62f3d8847e99d46
Minecrft-101 <taggertallred@gmail.com>
2024-02-15 14:40:47 -0800

    gg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWristCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoWristCheck.java
@@ -6,24 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.RobotConstants;
+import org.firstinspires.ftc.teamcode.SubSys.Claw;
 
 @TeleOp(name = "ServoWristCalibrate", group = "_JVBot")
 public class servoWristCheck extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo c = hardwareMap.get(Servo.class, "wristRight");
-        Servo d = hardwareMap.get(Servo.class, "wristLeft");
-
+        Claw claw = new Claw(hardwareMap.get(Servo.class, "handTop"), hardwareMap.get(Servo.class, "handBottom"), hardwareMap.get(Servo.class, "wristLeft"), hardwareMap.get(Servo.class, "wristRight"));
 
         waitForStart();
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                c.setPosition(RobotConstants.wrist_dropOffPos);
-                d.setPosition(RobotConstants.wrist_dropOffPos);
-                telemetry.addData("Right servo pos", c.getPosition());
-                telemetry.addData("Left servo pos", c.getPosition());
-                telemetry.update();
+                claw.setWristPos(RobotConstants.wrist_dropOffPos);
             }
         }
     }
